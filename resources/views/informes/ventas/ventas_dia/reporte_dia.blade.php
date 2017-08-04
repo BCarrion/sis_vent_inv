95482fddab935b12e1dea5085427e982f698d392
BCarrion <bernardocarrionperez@MacBook-Pro-de-Bernardo.local>
2017-08-03 23:48:57 -0500

    Parcial Ventas día

--- resources/views/informes/ventas/ventas_dia/reporte_dia.blade.php
+++ resources/views/informes/ventas/ventas_dia/reporte_dia.blade.php
@@ -1,34 +1,55 @@
-<head>
-    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
-</head>
-<div class="row">
-  <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-    <h3>Reporte de Ventas: {{$fecha_reporte}}</h3>
-  </div>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> </title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <style media="screen">
+      tbody
+      {
+          width: 800%;
+          height: 100%;
+          background-color: red;
+      }
+    </style>
+  </head>
+  <body>
+      <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <h3>Reporte de Ventas: {{$fecha_reporte}}</h3>
+        </div>
+      </div>
+
+    <div class="row">
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="table-responsive">
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+          <table class="table table-stripped table-bordered table-condensed table-bordered">
+            <thead>
+              <th>Fecha</th>
+              <th>Cliente</th>
+              <th>Comprobante</th>
+              <th>Impuesto</th>
+              <th>Total</th>
+              <th>Estado</th>
+            </thead>
+            <tbody>
+            @foreach($ventas as $ven)
+            <tr>
+              <td>{{$ven->fecha_hora}}</td>
+              <td>{{$ven->nombre}}</td>
+              <td>{{$ven->tipo_comprobante.': '.$ven->serie_comprobante.'-'.$ven->num_comprobante}}</td>
+              <td>{{$ven->impuesto}}</td>
+              <td>{{$ven->total_venta}}</td>
+              <td>{{$ven->estado}}</td>
+            </tr>
+            @endforeach
+            </tbody>
+          </table>
+          </div>
+        </div>
 </div>
-<div class="row">
-  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-    <div class="table-responsive">
-      <table class="table table-stripped table-bordered table-condensed table-bordered">
-        <thead style="background-color: #A9D0F5">
-          <th>Fecha</th>
-          <th>Cliente</th>
-          <th>Comprobante</th>
-          <th>Impuesto</th>
-          <th>Total</th>
-          <th>Estado</th>
-        </thead>
-        @foreach($ventas as $ven)
-        <tr>
-          <td>{{$ven->fecha_hora}}</td>
-          <td>{{$ven->nombre}}</td>
-          <td>{{$ven->tipo_comprobante.': '.$ven->serie_comprobante.'-'.$ven->num_comprobante}}</td>
-          <td>{{$ven->impuesto}}</td>
-          <td>{{$ven->total_venta}}</td>
-          <td>{{$ven->estado}}</td>
-        </tr>
-        @endforeach
-      </table>
-    </div>
-    </div>
 </div>
+
+  </body>
--- resources/views/informes/ventas/ventas_general/reporte_general.blade.php
+++ resources/views/informes/ventas/ventas_general/reporte_general.blade.php
@@ -1,1 +1,11 @@
-<h3>Estoy generando el informe de ventas mensual</h3>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> </title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+  </head>
+  <body>
+
+  </body>
+</html>
