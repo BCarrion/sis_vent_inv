bdd96b04a2b5634a89bdba152924cb0be83504de
BCarrion <carrion.bernardo@gmail.com>
2017-08-10 11:40:27 -0500

    Adminlte publicado con rutas ok y modelos de entrust

--- config/app.php
+++ config/app.php
@@ -164,6 +164,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
         Acoustep\EntrustGui\EntrustGuiServiceProvider::class,
+        Acacha\AdminLTETemplateLaravel\Providers\AdminLTETemplateServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -231,7 +232,8 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'PDF' => Barryvdh\DomPDF\Facade::class,
-        'Entrust'   => Zizaco\Entrust\EntrustFacade::class, 
+        'Entrust'   => Zizaco\Entrust\EntrustFacade::class,
+        'AdminLTE' => Acacha\AdminLTETemplateLaravel\Facades\AdminLTE::class,
 
     ],
 
